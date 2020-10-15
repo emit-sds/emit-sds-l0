0b9985dfc68aa58719178de6eaf9ba67d49b607b
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2020-10-15 11:06:28 -0700

    Add initial versions of run script and packet count script

---
+++ packet_cnt_check.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+ 
+import argparse
+ 
+import emit.data_products as dp
+ 
+parser = argparse.ArgumentParser()
+parser.add_argument('infile')
+args = parser.parse_args()
+ 
+in_file = open(args.infile, 'rb')
+ 
+cnt = 0
+while True:
+    try:
+        dp.CCSDSPacket(in_file)
+        cnt += 1
+    except EOFError:
+        break
+ 
+print(cnt)
