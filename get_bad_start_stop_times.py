c567b5b810a87b7e5e8cc207b573f1a7f69c2bf3
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2022-05-24 13:52:20 -0700

    Fix data type on gps_times.

--- get_bad_start_stop_times.py
+++ get_bad_start_stop_times.py
@@ -53,7 +53,7 @@ def main():
     if index is None:
         raise RuntimeError("Unable to find coarse time column (LADP06MD2378W) in data")
 
-    gps_times = [row[index] for row in rows]
+    gps_times = [float(row[index]) for row in rows]
 
     start_time = get_utc_time_from_gps(min(gps_times))
     stop_time = get_utc_time_from_gps(max(gps_times))
