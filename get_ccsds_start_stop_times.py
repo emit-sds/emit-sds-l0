4b8715eb06e07868ae1ee3ab9f6097f133214620
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2025-11-04 12:59:25 -0800

    Update python to 3.10, add new conda env, update ait.core.dmc calls.

--- get_ccsds_start_stop_times.py
+++ get_ccsds_start_stop_times.py
@@ -43,7 +43,7 @@ def main():
                 # Get coarse time and convert to UTC
                 coarse_time = int.from_bytes(pkt.body[:4], "big")
                 d = dmc.GPS_Epoch + datetime.timedelta(seconds=coarse_time)
-                offset = dmc.LeapSeconds.get_GPS_offset_for_date(d)
+                offset = dmc.LeapSeconds.get_gps_offset_for_date(d)
                 utc_time = d - datetime.timedelta(seconds=offset)
 
                 if start_time is None:
